0b7bcdafe487ce35dc2b6e6c7db424d7ccf30e21
faizan2700 <syedfaizan824@gmail.com>
2024-12-17 21:51:09 +0530

    updated middlewares

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI 
 from fastapi.responses import HTMLResponse 
-from middleware import AutoErrorHandler 
+# from middleware import AutoErrorHandler 
 from routes import router 
 
 
-app1 = FastAPI()  
-app1.include_router(router, prefix="/api")
+app = FastAPI()  
+app.include_router(router, prefix="/api")
 
-@app1.get("/") 
+@app.get("/") 
 def home(): 
     return HTMLResponse('<h1>Hello World</h1>')  
 
-app = AutoErrorHandler(app1) 
+# app = AutoErrorHandler(app) 
