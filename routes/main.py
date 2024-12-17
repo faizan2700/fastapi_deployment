52d6ca5b2f7823a2b4ed13a75461b197760b676a
faizan2700 <syedfaizan824@gmail.com>
2024-12-17 21:12:25 +0530

    updated code

--- routes/main.py
+++ routes/main.py
@@ -14,10 +14,12 @@ async def verification_for_webhook(request: Request):
 
 @router.post('/webhook') 
 async def handle_webhook_payload(request: Request): 
-    return {'status': 200, 'message': request.json()} 
+    payload = await request.json() 
+    return {'status': 200, 'message': payload} 
 
 @router.post('/send_message') 
 async def send_whatsapp_message(request: Request): 
-    return {'status': 200, 'message': request.json()} 
+    payload = await request.json()  
+    return {'status': 200, 'message': payload} 
 
  
