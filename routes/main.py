ea3f44d44b7617142b1c6692ae19f7194f9466e0
faizan2700 <syedfaizan824@gmail.com>
2024-12-17 22:33:15 +0530

    updated whatsapp service message

--- routes/main.py
+++ routes/main.py
@@ -4,10 +4,10 @@ import requests
 from dotenv import load_dotenv 
 from whatsapp_service import WhatsappService
 from Agent import Assistant 
-from fastapi.responses import HTMLResponse 
+from fastapi.responses import HTMLReponse 
 
 router = APIRouter() 
-
+whatsapp_service = WhatsappService() 
 @router.get('/webhook') 
 async def verification_for_webhook(request: Request): 
     return {'status': 200, 'message': 'Verified!'} 
@@ -20,6 +20,7 @@ async def handle_webhook_payload(request: Request):
 @router.post('/send_message') 
 async def send_whatsapp_message(request: Request): 
     payload = await request.json()  
-    return {'status': 200, 'message': payload} 
+    response = whatsapp_service.send_message('+919579591713', message_body='hello world!!!') 
+    return {'status': 200, 'message': response} 
 
  
