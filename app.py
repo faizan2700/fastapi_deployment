897a810b529a73208aeb7746f668534a0fa6166c
faizan2700 <syedfaizan824@gmail.com>
2024-12-18 17:49:13 +0530

    updated code

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI 
 from fastapi.responses import HTMLResponse 
-# from routes import router 
-
 
 app = FastAPI()  
-# app.include_router(router, prefix="/api")
 
 @app.get("/") 
 def home(): 
