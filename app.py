390be3858d5c23cb9d958545e989609d52d096ad
faizan2700 <syedfaizan824@gmail.com>
2024-12-18 18:09:49 +0530

    updated code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI 
 from fastapi.responses import HTMLResponse 
+from routes import router 
 
 app = FastAPI()  
+app.include_router(router, prefix='api/') 
 
 @app.get("/") 
 def home(): 
     return HTMLResponse('<h1>Hello World</h1>')  
-
+ 
 
