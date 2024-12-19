47d77caa37ab5d4ced879c3d5e156f4eea2f21f1
faizan2700 <syedfaizan824@gmail.com>
2024-12-19 19:28:22 +0530

    updated webhoo handling code

--- routes/main.py
+++ routes/main.py
@@ -34,18 +34,15 @@ async def handle_webhook_payload(request: Request):
     print(f'webhook received num:{num}, msg:{msg}') 
     if num is None or msg is None: 
         return {'status': 200} 
-    whatsapp_service.send_message(num, msg) 
+    resp = assitant.get_response(msg) 
+    whatsapp_service.send_message(num, resp) 
     return {'status': 200} 
 
 @router.post('/send_message') 
 async def send_whatsapp_message(request: Request): 
-    data = await request.json() 
-    try:  
-        num, msg = data.get('number'), data.get('message') 
-    except Exception: 
-        raise HTTPException(status_code=400, detail='number and message are necessary!') 
-    resp = assitant.get_response(msg) 
-    response = whatsapp_service.send_message(num, message_body=resp) 
+    data = await request.json()
+    num, msg = data.get('number'), data.get('message') 
+    response = whatsapp_service.send_message(num, msg) 
     return {'status': 200, 'message': response} 
 
 @router.post('/get_response') 
