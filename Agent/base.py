825a60edbc411a4ecbe56d0742ff7944ea414f9f
faizan2700 <syedfaizan824@gmail.com>
2024-12-20 12:13:18 +0530

    updated agent

--- Agent/base.py
+++ Agent/base.py
@@ -4,7 +4,8 @@ class Assistant:
     def __init__(self, model_name: str = "google-t5/t5-large"):
         self.model = pipeline("text2text-generation", model=model_name) 
     def get_response(self, user_input: str) -> str:
-        return self.model(user_input)[0].get('generated_text') 
+        # return self.model(user_input)[0].get('generated_text') 
+        return user_input 
     
 if __name__=='__main__': 
     assistant = Assistant()
